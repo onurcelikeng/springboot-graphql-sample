bb7d3a3f5fa89c80a5dfc72687950a119787ebfc
onurcelikeng <onrcelik@outlook.com>
2020-10-21 22:35:06 +0300

    init commit

---
+++ src/main/java/com/onurcelikeng/springbootgraphqlsample/SpringBootGraphQlSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onurcelikeng.springbootgraphqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphQlSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphQlSampleProjectApplication.class, args);
+	}
+
+}
